e61f26103ad7a4ffba082d0fb03f407e2741fcbf
gschri <gabrielschri@gmail.com>
2022-05-13 10:12:38 -0300

    Add css loading feature and refactor caching logic

--- packages/local-client/src/plugins/fetch-plugin.ts
+++ packages/local-client/src/plugins/fetch-plugin.ts
@@ -6,17 +6,55 @@ export let fecthPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
+      build.onLoad({ filter: /^(index\.js)$/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode
         }
+      })
+
+      // return cached file if found
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         let cachedResult = await localForage.getItem<esbuild.OnLoadResult>(args.path);
         if (cachedResult) {
           return cachedResult
         }
+        return null
+      })
+
+      build.onLoad({ filter: /\.css$/ }, async (args: any) => {
+        let { data, request } = await axios.get(args.path)
+
+
+        let escaped = data
+          .replace(/\n/g, '') // collapse CSS into a single line
+          .replace(/"/g, '\\"') // escape double-quotes
+          .replace(/'/g, "\\'") // escape single-quotes
+
+        let contents = `
+            let style = document.createElement('style')
+            style.innerText = '${escaped}';
+            document.head.appendChild(style)
+          `
+
+        let result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname
+        }
+
+        await localForage.setItem(args.path, result)
+
+        return result
+
+      })
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        let cachedResult = await localForage.getItem<esbuild.OnLoadResult>(args.path);
+        if (cachedResult) {
+          return cachedResult
+        }
+
         let { data, request } = await axios.get(args.path)
 
         let result: esbuild.OnLoadResult = {
