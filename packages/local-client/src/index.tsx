361f9ca9220e2802002888e65687705d5adadde4
gschri <gabrielschri@gmail.com>
2022-05-13 10:18:25 -0300

    Remove local esbuild.wasm file and load it directly from unpkg

--- packages/local-client/src/index.tsx
+++ packages/local-client/src/index.tsx
@@ -12,7 +12,7 @@ let App = () => {
   var startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     })
   }
   useEffect(() => {
